1c0ef4c2f4f5176a004b5435fc142be7f17fb558
zeshan <zeshanshakil0@gmail.com>
2024-08-12 13:25:46 +0500

    feat: tenant middleware

---
+++ api/auth/middleware/tenant.middleware.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.co/golang-programming/restaurant/api/database"
+)
+
+func TenantMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tenantID := r.Header.Get("X-Tenant-ID") // Or derive it from URL, subdomain, etc.
+
+		err := database.SetDB(tenantID)
+		if err != nil {
+			http.Error(w, "Tenant not found", http.StatusNotFound)
+			return
+		}
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
