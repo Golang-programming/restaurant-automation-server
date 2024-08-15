059f331edd3402bb9b8e8a7fc60cf4d1da736bed
zeshan <zeshanshakil0@gmail.com>
2024-08-15 15:59:43 +0500

    feat: redis tavle

--- api/customer/utils/helper.go
+++ api/customer/utils/helper.go
@@ -1,69 +1,13 @@
-/* package utils
-
-import (
-	"fmt"
-
-
-	"github.co/golang-programming/restaurant/api/redis"
-)
-
-const activeUsersKey = "active_users"
-
-func AddActiveUser(userID uint) error {
-	// Convert the userID to a string
-	member := fmt.Sprintf("%d", userID)
-
-	// Add the userID to the Redis set
-	err := redis.SAdd(activeUsersKey, member)
-	if err != nil {
-		return fmt.Errorf("failed to add active user: %v", err)
-	}
-	return nil
-}
-
-func RemoveActiveUser(userID uint) error {
-	// Convert the userID to a string
-	member := fmt.Sprintf("%d", userID)
-
-	// Remove the userID from the Redis set
-	err := redis.SRem(activeUsersKey, member)
-	if err != nil {
-		return fmt.Errorf("failed to remove active user: %v", err)
-	}
-	return nil
-}
-
-func GetActiveUsers() ([]uint, error) {
-	// Get all active userIDs from the Redis set
-	members, err := redis.GetRedisClient().SMembers(activeUsersKey).Result()
-	if err != nil {
-		return nil, fmt.Errorf("failed to get active users: %v", err)
-	}
-
-	// Convert the result from strings to uints
-	var userIDs []uint
-	for _, member := range members {
-		var userID uint
-		_, err := fmt.Sscanf(member, "%d", &userID)
-		if err != nil {
-			return nil, fmt.Errorf("failed to parse userID: %v", err)
-		}
-		userIDs = append(userIDs, userID)
-	}
-
-	return userIDs, nil
-}
-*/
-
 package utils
 
 import (
 	"fmt"
-	"github.co/golang-programming/restaurant/api/redis"
 	"strconv"
+
+	"github.co/golang-programming/restaurant/api/redis"
 )
 
-const activeUsersKey = "active_users"
+const activeUsersKey = "users:active"
 
 func AddActiveUser(userID uint) error {
 	return redis.SAdd(activeUsersKey, userID)
